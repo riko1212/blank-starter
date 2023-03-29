ca5aa547d36c1895a501647cfafc461bc1684daf
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-03-29 11:19:16 +0300

    st

--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-const randomColor = () =>
-  '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
-
-const paintingSectionsInRandomColor = () => {
-  document.querySelectorAll('.section').forEach((section) => {
-    section.style.backgroundColor = randomColor();
-  });
-};
