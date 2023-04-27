19a158e0ead20908b7988c4c0620b5f4eb7517d1
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-27 09:28:40 +0300

    +++

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,14 +1,1 @@
-fetch('https://jsonplaceholder.typicode.com/photos/1')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
 
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,18 +1,16 @@
 // https://openweathermap.org/current
-import { fetchCurrentWeather } from './weather-api.js';
-import createWeatherCard from '../templates/weather-card.hbs';
 
 const weatherFormEl = document.querySelector('.js-search-form');
 const weatherWrapperEl = document.querySelector('.js-weather__wrapper');
 
-const convertSecondsToHoursAndMinutes = seconds => {
-  const date = new Date(seconds * 1000);
+// const convertSecondsToHoursAndMinutes = seconds => {
+//   const date = new Date(seconds * 1000);
 
-  return `${new String(date.getHours()).padStart(2, 0)}:${new String(date.getMinutes()).padStart(
-    2,
-    0
-  )}`;
-};
+//   return `${new String(date.getHours()).padStart(2, 0)}:${new String(date.getMinutes()).padStart(
+//     2,
+//     0
+//   )}`;
+// };
 
 const onWeatherFormSubmit = event => {
   event.preventDefault();
