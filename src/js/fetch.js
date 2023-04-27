54ec63b1a18c716764b8af3b63baadfa5a20dcb8
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-27 07:03:50 +0300

    +++

---
+++ src/js/fetch.js
@@ -0,0 +1,14 @@
+fetch('https://jsonplaceholder.typicode.com/photos/1')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
