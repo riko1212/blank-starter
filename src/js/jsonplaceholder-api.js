50352be3c87d98fca7980bd76402526fe23a499f
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-28 09:17:07 +0300

    +++

--- src/js/jsonplaceholder-api.js
+++ src/js/jsonplaceholder-api.js
@@ -1,19 +1,1 @@
 'use strict';
-
-export class JsonPlaceholderApi {
-  #BASE_URL = 'https://jsonplaceholder.typicode.com';
-
-  constructor() {
-    this.page = 1;
-  }
-
-  fetchPosts() {
-    return fetch(`${this.#BASE_URL}/posts?_page=${this.page}&_limit=10`).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      return response.json();
-    });
-  }
-}
