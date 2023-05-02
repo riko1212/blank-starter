1d3d57204543a1e02a0ddc43364c92f7cc7a560b
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-05-02 07:11:22 +0300

    +++

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,129 +1,77 @@
 // https://unsplash.com/
+import createGalleryCard from '../templates/gallery-card.hbs';
 import { UnsplashApi } from './unsplash-api';
-import createGalleryCards from '../templates/gallery-card.hbs';
-
-const searchFormEl = document.querySelector('.js-search-form');
-const galleryListEl = document.querySelector('.js-gallery');
-const loadMoreBtnEl = document.querySelector('.js-load-more');
 
 const unsplashApi = new UnsplashApi();
 
-const paintRandomPhotosByPageLoad = async () => {
-  // Варіант через then()/catch()
-  // unsplashApi
-  //   .fetchRandomPhotos()
-  //   .then(response => {
-  //     const { data } = response;
-
-  //     galleryListEl.innerHTML = createGalleryCards(data);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-
-  // Варіант через async/await
-  try {
-    const { data } = await unsplashApi.fetchRandomPhotos();
-
-    galleryListEl.innerHTML = createGalleryCards(data);
-  } catch (err) {
-    console.log(err);
-  }
+const showRandomPhotos = () => {
+  unsplashApi
+    .getRandomPhotos()
+    .then(response => {
+      const { data } = response;
+      console.log(data);
+      galleryListEl.innerHTML = createGalleryCard(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
-paintRandomPhotosByPageLoad();
+showRandomPhotos();
 
-const onLoadMoreBtnElClick = async event => {
-  // Варіант через then()/catch()
-  // unsplashApi.page += 1;
-  // unsplashApi
-  //   .fetchPhotosByQuery()
-  //   .then(response => {
-  //     const { data } = response;
-  //     galleryListEl.insertAdjacentHTML('beforeend', createGalleryCards(data.results));
-  //     if (data.total_pages === unsplashApi.page) {
-  //       loadMoreBtnEl.classList.add('is-hidden');
-  //       loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnElClick);
-  //     }
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
+const searchFormEl = document.querySelector('.js-search-form');
+const loadMoreBtnEl = document.querySelector('.js-load-more');
+const galleryListEl = document.querySelector('.js-gallery');
 
-  // Варіант через async/await
-  try {
-    unsplashApi.page += 1;
-
-    const { data } = await unsplashApi.fetchPhotosByQuery();
-
-    galleryListEl.insertAdjacentHTML('beforeend', createGalleryCards(data.results));
-
-    if (data.total_pages === unsplashApi.page) {
-      loadMoreBtnEl.classList.add('is-hidden');
-      loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnElClick);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const onSearchFormElSubmit = async event => {
-  event.preventDefault();
-
-  unsplashApi.searchQuery = event.currentTarget.elements.user_search_query.value;
+const onSearchFormSubmit = e => {
+  e.preventDefault();
   unsplashApi.page = 1;
-  loadMoreBtnEl.classList.add('is-hidden');
+  unsplashApi.searchQuery = e.currentTarget.user_search_query.value;
+  unsplashApi
+    .fetchPhotosByQuery()
+    .then(response => {
+      const { data } = response;
+      console.log(data);
+      if (data.total_pages === 0) {
+        galleryListEl.innerHTML = '';
+        loadMoreBtnEl.classList.add('is-hidden');
+        loadMoreBtnEl.removeEventListener('click', onMoreBtnClick);
+        console.log('За вашим запитом нічого не знайдено');
+        return;
+      }
 
-  // Варіант через then()/catch()
-  // unsplashApi
-  //   .fetchPhotosByQuery()
-  //   .then(response => {
-  //     console.log(response);
-  //     const { data } = response;
-
-  //     if (data.total_pages === 0) {
-  //       loadMoreBtnEl.classList.add('is-hidden');
-  //       galleryListEl.innerHTML = '';
-  //       return;
-  //     }
-
-  //     if (data.total_pages === 1) {
-  //       galleryListEl.innerHTML = createGalleryCards(data.results);
-  //       loadMoreBtnEl.classList.add('is-hidden');
-
-  //       return;
-  //     }
-
-  //     galleryListEl.innerHTML = createGalleryCards(data.results);
-  //     loadMoreBtnEl.classList.remove('is-hidden');
-  //     loadMoreBtnEl.addEventListener('click', onLoadMoreBtnElClick);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-
-  // Варіант через async/await
-  try {
-    const { data } = await unsplashApi.fetchPhotosByQuery();
-
-    if (data.total_pages === 0) {
-      galleryListEl.innerHTML = '';
-
-      return;
-    }
-
-    if (data.total_pages === 1) {
-      galleryListEl.innerHTML = createGalleryCards(data.results);
-
-      return;
-    }
-
-    galleryListEl.innerHTML = createGalleryCards(data.results);
-    loadMoreBtnEl.classList.remove('is-hidden');
-    loadMoreBtnEl.addEventListener('click', onLoadMoreBtnElClick);
-  } catch (err) {
-    console.log(err);
-  }
+      if (data.total_pages === 1) {
+        loadMoreBtnEl.classList.add('is-hidden');
+        loadMoreBtnEl.removeEventListener('click', onMoreBtnClick);
+        galleryListEl.innerHTML = createGalleryCard(data.results);
+        return;
+      }
+      galleryListEl.innerHTML = createGalleryCard(data.results);
+      loadMoreBtnEl.classList.remove('is-hidden');
+      loadMoreBtnEl.addEventListener('click', onMoreBtnClick);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
-searchFormEl.addEventListener('submit', onSearchFormElSubmit);
+const onMoreBtnClick = () => {
+  unsplashApi.page += 1;
+
+  unsplashApi
+    .fetchPhotosByQuery()
+    .then(response => {
+      const { data } = response;
+      console.log(data.results);
+      galleryListEl.insertAdjacentHTML('beforeend', createGalleryCard(data.results));
+      if (unsplashApi.page === data.total_pages) {
+        loadMoreBtnEl.classList.add('is-hidden');
+        loadMoreBtnEl.removeEventListener('click', onMoreBtnClick);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -1,20 +1,20 @@
 'use strict';
-import axios from 'axios';
 
+import axios from 'axios';
 export class UnsplashApi {
   #BASE_URL = 'https://api.unsplash.com';
-  #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
-
+  #API_KEY = 'Xn_eH2Gi-hkj7ulpwwCcpyWldnnOhEEXSryD5-QYcnQ';
   constructor() {
     this.page = null;
     this.searchQuery = '';
+    this.perPage = 12;
   }
 
   fetchPhotosByQuery() {
     const searchParams = {
       query: this.searchQuery,
       page: this.page,
-      per_page: 12,
+      per_page: this.perPage,
       orientation: 'portrait',
       client_id: this.#API_KEY,
     };
@@ -22,13 +22,13 @@ export class UnsplashApi {
     return axios.get(`${this.#BASE_URL}/search/photos`, { params: searchParams });
   }
 
-  fetchRandomPhotos() {
+  getRandomPhotos() {
     const searchParams = {
+      page: this.page,
       orientation: 'portrait',
-      count: 12,
       client_id: this.#API_KEY,
+      count: 12,
     };
-
     return axios.get(`${this.#BASE_URL}/photos/random`, { params: searchParams });
   }
 }
